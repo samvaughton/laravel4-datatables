0c616c3b248be5a598b78fa9962b00617dfff9ae
Sam Vaughton <samvaughton@gmail.com>
2013-12-21 17:06:45 +0000

    Removed test for now.

--- tests/TestDataTable.php
+++ tests/TestDataTable.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use Samvaughton\Ldt\DataTable;
 
 class TestDataTable extends PHPUnit_Framework_TestCase
@@ -15,34 +14,6 @@ class TestDataTable extends PHPUnit_Framework_TestCase
                 \Mockery::mock('\Samvaughton\Ldt\Column'),
             )
         );
-    }
-
-    public function testMake()
-    {
-        return;
-        $dt = new DataTable(
-            \Mockery::mock('\Samvaughton\Ldt\Builder\BuilderInterface'),
-            \Mockery::mock('\Samvaughton\Ldt\Request'),
-            array(
-                \Mockery::mock('\Samvaughton\Ldt\Column', function ($mock) {
-                    /** @var \Mockery\Mock $mock */
-
-                }),
-            )
-        );
-
-        $actual = $dt->make(true);
-        $expected = json_encode(array(
-            'sEcho' => 4,
-            'iTotalDisplayRecords' => 20,
-            'iTotalRecords' => 20,
-            'aaData' => array(
-                array('john', 15),
-                array('jane', 14),
-            )
-        ));
-
-        $this->assertEquals($expected, $actual);
     }
 
     public function tearDown()
