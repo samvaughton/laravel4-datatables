3221d4f20efc84e74b256fa599aac255de4100c0
Sam Vaughton <samvaughton@gmail.com>
2014-04-06 10:05:51 +0100

    Added fix to remove all invalid UTF-8 characters.

--- src/Samvaughton/Ldt/DataTable.php
+++ src/Samvaughton/Ldt/DataTable.php
@@ -141,7 +141,9 @@ class DataTable
                     continue;
                 }
 
-                $filteredRow[$column] = $displayColumn->callRowProcessor($value, $filteredRow, $rowData);
+                $filteredRow[$column] = $this->stripInvalidChars(
+                    $displayColumn->callRowProcessor($value, $filteredRow, $rowData)
+                );
             }
 
             $filtered[] = $this->stripColumnKeys($filteredRow);
@@ -150,6 +152,36 @@ class DataTable
         return $filtered;
     }
 
+    /**
+     * Strips invalid UTF-8 characters that can cause issues with json_encode()
+     *
+     * @param string $string
+     * @return string
+     */
+    private function stripInvalidChars($string)
+    {
+        if (is_null($string) || empty($string) || $string == false) {
+            return;
+        }
+
+        // Reject overly long 2 byte sequences, as well as characters above U+10000 and replace with ''
+        $string = preg_replace('/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.
+            '|[\x00-\x7F][\x80-\xBF]+'.
+            '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'.
+            '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'.
+            '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
+            '', $string);
+
+        // Reject overly long 3 byte sequences and UTF-16 surrogates and replace with ''
+        $string = preg_replace('/\xE0[\x80-\x9F][\x80-\xBF]'.
+            '|\xED[\xA0-\xBF][\x80-\xBF]/S','', $string);
+
+        $string = iconv('UTF-8', 'UTF-8//IGNORE', $string);
+
+        $string = str_replace('�', '', $string);
+
+        return $string;
+    }
 
     /**
      * Converts the column numbers to actual column instances.
